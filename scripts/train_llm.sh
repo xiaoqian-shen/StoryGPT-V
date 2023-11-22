abf31350be02d76b44ed0ea0b0a2d2153f184d7f
XiaoqianShen <64844805+xiaoqian-shen@users.noreply.github.com>
2023-11-22 14:48:41 +0300

    Update and rename train_gill.sh to train_llm.sh

--- scripts/train_llm.sh
+++ scripts/train_llm.sh
@@ -6,7 +6,7 @@ run_time="${current_month}-${current_day}-${current_hour}"
 DATASET=$1
 llm_model=$2
 
-python -u train_gill.py \
+python -u train_llm.py \
     --exp-name ${run_time}_${DATASET_NAME}_interleave_fuse --log-base-dir='logs/gill/' \
     --precision='bf16'  --print-freq=100 --batch-size=64  --val-batch-size=64 \
     --dataset-dir data/${DATASET} \
